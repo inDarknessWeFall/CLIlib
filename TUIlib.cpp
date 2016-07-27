2ab868138f2b5a196c73db1e8aff89c7bdf45a8b
inDarknessWeFall <dimakozin@gmail.com>
2016-07-27 14:18:25 +0300

    Window's title

--- TUIlib.cpp
+++ TUIlib.cpp
@@ -20,7 +20,10 @@ void Window::createFrame()
 
 Window::Window(const int& height = 0, const int& width = 0, const int& startY = 0, const int& startX = 0)
 {
+		startPoint.Y = startY;
+		startPoint.X = startX;
 		currentWindow = createNewWindow(height,width,startY,startX);
+		getmaxyx(currentWindow, maxY,maxX);
 }
 
 Window::~Window()
@@ -28,7 +31,21 @@ Window::~Window()
 		delwin(currentWindow);
 }
 
-void Window::setTitle(char title[length])
+void Window::printTitle(unsigned int point, const char* title)
 {
-	
+		mvwaddch(currentWindow, 0, point, ACS_RTEE);
+		waddch(currentWindow, ' ');
+		waddstr(currentWindow, title);
+		waddch(currentWindow, ' ');
+		waddch(currentWindow, ACS_LTEE);
+		touchwin(currentWindow);
+		wrefresh(currentWindow);
+}
+
+void Window::setTitle(const char* title)
+{
+		this->title = title;
+		unsigned int stringSize = 4 + strlen(title);		
+		unsigned int temp = (maxX - stringSize)/2;
+		printTitle(temp,title);
 }
--- TUIlib.h
+++ TUIlib.h
@@ -13,37 +13,37 @@
 ////////////////
 
 #include <ncurses.h>
-
-#define length 24
-
-using namespace std;
+#include <string.h>
 
 class CWidget
 {
 /////////////////
 // Main class  //
 /////////////////
-		private:
+		protected:
 				struct Point
 				{
 						unsigned int Y;
 						unsigned int X;
 				};
-
+				Point startPoint;
 };
 
 class Window : public CWidget
 {
 	private:
 			WINDOW* currentWindow;
+			const char* title;
+			int maxY;
+			int maxX;
+
 			WINDOW* createNewWindow(const int& height = 0, const int& width = 0, const int& startY = 0, const int& startX = 0);
 			inline void update();
-			char title[length];
-
+			inline void printTitle(unsigned int point, const char* title);
 	public:
 			Window(const int& height, const int& width, const int& startY, const int& startX);
 			~Window();
-			void setTitle(char title[length]);			
+			void setTitle(const char* title);			
 			void createFrame();
 			WINDOW* returnWINDOW();
 };
